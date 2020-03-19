7a68bf0869a2a91fb95e32bbdc27cf460109c67b
kosytedy <kosytedy@yahoo.com>
2020-03-19 09:28:17 +0000

    Initial commit

---
+++ src/test/java/com/kosytedy/uthinkuknowme/UthinkUknowMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosytedy.uthinkuknowme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UthinkUknowMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
